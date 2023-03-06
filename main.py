d02d338aa89817cef3c4b3b3ed17125f9637d47b
sreejith <squeeky.mouse@gmail.com>
2023-03-06 20:45:00 +0530

    impl Res class functions Sec41

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas
-# commit: impl Hotel class functions Sec41
+# commit: impl Res class functions Sec41
 
 df=pandas.read_csv('hotels.csv',dtype={'id':str}) # load all values as str
 
@@ -7,6 +7,7 @@ df=pandas.read_csv('hotels.csv',dtype={'id':str}) # load all values as str
 class Hotel:
     def __init__(self,hotel_id) -> None:
         self.hotel_id=hotel_id
+        self.name=df.loc[df['id']==hotel_id,'name'].squeeze()
         pass
         
     def book(self): # book what? a hotel not user!!!
@@ -25,12 +26,18 @@ class Hotel:
 
 class ReservationTicket:
     def __init__(self,cust_name,hotel_object) -> None:
-        pass
+        self.cust_name=cust_name
+        self.hotel=hotel_object
 
     def generate(self):
-        # content=f'Name of the customer hotel'
-        # return(content)
-        pass
+        content=f"""
+        Thank you for your reservation!
+        Here are your booking data:
+        Name: {self.cust_name}
+        Hotel: {self.hotel.name}
+        """
+        return(content)
+        
 
 
 print(df)
@@ -40,7 +47,7 @@ hotel=Hotel(hotel_ID)
 if hotel.available():
     hotel.book()
     name=input('Enter your name: ')
-    reservation_ticket=ReservationTicket(name,hotel)
+    reservation_ticket=ReservationTicket(cust_name=name,hotel_object=hotel)
     print(reservation_ticket.generate())
 else:
     print('Hotel is not free.')
